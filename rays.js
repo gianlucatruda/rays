c18220a24e4be47b5287636767027f8991641356
Gianluca Truda <gianlucatruda@gmail.com>
2024-05-07 18:45:50 +0100

    Updated scene parameters. 5 Spheres.

--- rays.js
+++ rays.js
@@ -76,7 +76,7 @@ class Color {
 }
 
 const SCENE = {
-    lights: [{ x: -30, y: -10, z: 20 }],
+    lights: [{ x: -40, y: -10, z: 40 }],
     objects: [
         {
             type: "sphere",
@@ -84,16 +84,16 @@ const SCENE = {
             radius: 2.5,
             color: new Color(128, 128, 128),
             specular: 0.0,
-            lambert: 0.9,
+            lambert: 0.8,
             ambient: 0.3,
         },
         {
             type: "sphere",
             position: { x: 1.2, y: 2, z: -1 },
             radius: 1.0,
-            color: new Color(100, 120, 100),
-            specular: 0.99, // high spec object
-            lambert: 0.9,
+            color: new Color(100, 110, 100),
+            specular: 0.999, // high spec object
+            lambert: 0.7,
             ambient: 0.3,
         },
         {
@@ -104,6 +104,24 @@ const SCENE = {
             specular: 0.5,
             lambert: 0.8,
             ambient: 0.2,
+        },
+        {
+            type: "sphere",
+            position: { x: 3.0, y: 6, z: -5 },
+            radius: 4,
+            color: new Color(255, 255, 255),
+            specular: 0.2,
+            lambert: 0.7,
+            ambient: 0.3,
+        },
+        {
+            type: "sphere",
+            position: { x: -0.4, y: 6, z: 0 },
+            radius: 0.5,
+            color: new Color(100, 110, 100),
+            specular: 0.98, // high spec object
+            lambert: 0.8,
+            ambient: 0.3,
         },
     ],
 };
